5ac9577a6b17da8f9cb16d7f5f41591fe57e86da
Nutty4616 <nkosin361@gmail.com>
2022-03-08 14:57:22 +0200

    Code Refactor

--- example/main.dart
+++ example/main.dart
@@ -2,11 +2,11 @@ import 'package:kingcli_nn/src/option.dart';
 import 'package:kingcli_nn/src/prompter.dart';
 
 void main() {
-  var options = [
+  final options = [
     new Option('I want red', '#f00'),
     new Option('I want blue', '#00f')
   ];
 
-  var prompter = new Prompter();
-  prompter.ask('What color do you want', options);
+  final prompter = new Prompter();
+  print(prompter.askBinary('Do you like Dart?'));
 }
